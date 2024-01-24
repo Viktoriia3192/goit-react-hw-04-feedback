beacb6d3c806e9b2bccf1cf87ae03a1efeef2673
Viktoriia3192 <127699121+Viktoriia3192@users.noreply.github.com>
2024-01-24 11:47:19 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import { Section } from "./Section/Section";
+import { Statistic } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Notification } from "./Notification/Notification";
+import { MainWrap } from "./App.styled";
+import { Component } from "react";
+export class App extends Component{
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+        countTotalFeedback({good, neutral, bad}){
+       return good + neutral + bad
+      }
+
+      countPositiveFeedbackPercentage(goodOnes){
+        const allFeedbacks = this.countTotalFeedback(this.state)
+        const result = Math.round(goodOnes * 100 / allFeedbacks)
+        return result 
+      }
+
+      onLeavefeedback = (option) => {
+     
+        this.setState((prevState)=> ({
+            [option]: prevState[option] + 1
+        }))
+      }
+
+  render(){
+    const total = this.countTotalFeedback(this.state);
+    const positivePercentage = this.countPositiveFeedbackPercentage(this.state.good);
+  return (
+    <MainWrap>
+    <Section title={'Please leave feedback'}>
+    <FeedbackOptions options={Object.keys(this.state)} onLeavefeedback={this.onLeavefeedback}/>
+    </Section>
+    <Section title={'Statistic'}>
+      {total === 0 ? <Notification Textmessage="There is no feedback"/> :
+    <Statistic 
+    total={total}
+    positivePercentage={positivePercentage}
+    data={this.state}/>}</Section>
+    </MainWrap>
+  );}
+};
+
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SectionWrap = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center
+
+`
+
+export const Title = styled.h2`
+color: #4d725a;
+`
